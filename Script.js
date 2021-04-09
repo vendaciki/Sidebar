d29ebc08a98f609909e9577b209ee4df93c5788e
vendaciki <50072566+vendaciki@users.noreply.github.com>
2021-04-09 11:18:43 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
